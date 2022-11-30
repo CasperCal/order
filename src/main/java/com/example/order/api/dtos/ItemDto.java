abfd05bcd826d49a78871a1979dc346026f9c8bc
Casper Caluwe <casper.c@live.be>
2022-11-30 11:21:19 +0100

    added working order addItem test

--- src/main/java/com/example/order/api/dtos/ItemDto.java
+++ src/main/java/com/example/order/api/dtos/ItemDto.java
@@ -1,4 +1,18 @@
 package com.example.order.api.dtos;
 
+import java.util.Objects;
+
 public record ItemDto(String id, String name, String description, double price, int amount) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemDto itemDto = (ItemDto) o;
+        return Double.compare(itemDto.price, price) == 0 && amount == itemDto.amount && Objects.equals(name, itemDto.name) && Objects.equals(description, itemDto.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, price, amount);
+    }
 }
--- src/test/java/com/example/order/api/ItemControllerTests.java
+++ src/test/java/com/example/order/api/ItemControllerTests.java
@@ -1,4 +1,43 @@
 package com.example.order.api;
 
+import com.example.order.api.dtos.ItemDto;
+import com.example.order.domain.repos.ItemRepo;
+import io.restassured.RestAssured;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemControllerTests {
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private ItemRepo itemrepo;
+
+    @DisplayName("adding item tests")
+    @Nested
+    class AddItemTests{
+        @Test
+        void createItem_asAdminWithCorrectBody_ReturnsCorrectItemDto(){
+            JSONObject requestParams = new JSONObject();
+            requestParams.put("name","Unit test item");
+            requestParams.put("description", "an item generated in unit tests");
+            requestParams.put("price", 20.4);
+            requestParams.put("amount", 1);
+
+            ItemDto result = RestAssured.given().port(port).auth().preemptive().basic("admin@test.code","pwd")
+                    .contentType("application/json").body(requestParams)
+                    .when().post("/items/add")
+                    .then().statusCode(201).and().extract().as(ItemDto.class);
+            assertEquals(new ItemDto(result.id(),"Unit test item", "an item generated in unit tests",20.4,1)
+                        , result);
+        }
+    }
 }
